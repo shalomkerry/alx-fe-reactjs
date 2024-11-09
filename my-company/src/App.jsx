d3c0c9085f80ab6deda661fc5ac32592c612b7fc
Shalom <youremail@gmail.com>
2024-11-10 01:53:59 +0300

    edited the app

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Services from "./components/Services";
 import "./App.css";
 
 function App() {
@@ -9,6 +12,12 @@ function App() {
     <>
       <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/service" element={<Services />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,8 +1,5 @@
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Contact from "./Contact";
-import About from "./About";
-import Services from "./Services";
+
 function Navbar() {
   return (
     <>
@@ -12,12 +9,6 @@ function Navbar() {
         <Link to="/contact">Contact</Link>
         <Link to="/service">Service</Link>
       </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/service" element={<Services />} />
-      </Routes>
     </>
   );
 }
