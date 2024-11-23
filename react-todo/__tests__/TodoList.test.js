b35bc1c984650f54fca36fce6e8f2c9279fa04e9
shalomkerry <shalombadeg8@gmail.com>
2024-11-24 00:22:22 +0300

    something

--- react-todo/__tests__/TodoList.test.js
+++ react-todo/__tests__/TodoList.test.js
@@ -1,12 +1,12 @@
 import {render,screen,fireEvent} from '@testing-library/jest-dom'
-import HomePage from '../src/pages/TodoList'
+import TodoList from '../src/pages/TodoList'
 test('render homePage',()=>{
-    render(HomePage)
+    render(TodoList)
     expect(screen.getAllByRole('paragraph')).toBeInTheDocument();
     expect(screen.getByText('Eat Water')).toBeInTheDocument();
 })
 test('check if action can be added',()=>{
-    render(HomePage)
+    render(TodoList)
     const input = screen.getByRole('textbox'); 
 
     fireEvent.keyDown(input, {key: 'A', code: 'KeyA', charCode: 13})
