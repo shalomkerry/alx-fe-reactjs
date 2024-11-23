953a9ba49dbc7b717efa313f1065fafcb7509514
shalomkerry <shalombadeg8@gmail.com>
2024-11-23 16:16:07 +0300

    mistake

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -11,36 +11,37 @@ function RegistrationForm(){
     function handleSubmit(e){
         e.preventDefault();
 
+if(!username){
+    setErrors('Errors')
+}
+if(!password){
+    setErrors('Errors')
+}
+if(!email){
+    setErrors('Errors')
+}
         console.log(username,email,password)
     }
-
     return(<>
     <NavBar/>
  <form onSubmit={handleSubmit} style={{color:'beige',display:"flex",flexDirection:'column',gap:'2em'}} >
     <label htmlFor="name">Enter Your User Name</label>
     <input type="text" name="name" value={username} onChange={(e)=>{
         setUsername(e.target.value);
-        if(!username){
-            error=true
-        }
-    }} required/>
+      
+    }} />
     
     <label htmlFor="email">Enter Your Email</label>
     <input type="email" name="email" value={email} onChange={(e)=>{
         setEmail(e.target.value)
-        if(!email){
-            error=true;
-        }
-    } }required/>
+      
+    } }/>
     <label htmlFor="password" >Enter Your Password</label>
     <input type="password" name="password" value={password} onChange={(e)=>{
         setPassword(e.target.value)
        
-    }} required/>
-    {function validate(){
-        if(!password){
-        return 'error'
-    }}}
+    }} />
+  
         <input type="submit" value='Submit' />
     </form>   
     </>)
