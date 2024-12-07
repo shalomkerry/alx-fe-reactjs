42ee7c12fe9ab478c415df2ec99e45fac1b11bf8
shalomkerry <shalombadeg8@gmail.com>
2024-12-07 15:05:06 +0300

    mock data creation

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -24,7 +24,7 @@ return(
 {recipe.items?(recipe.items.map((item)=>(<>
 
 <Link to={`/recipe`} state={{content:`${item.name}`}}>
-<div key={item.id} className='bg-indigo-100 flex flex-col w-[60%] hover:cursor-pointer overflow-hidden hover:shadow-[10px_20px_10px_rgba(90,90,90)] md:w-[500px] lg:w-[600px]  sm:w-[400px]  rounded-lg pb-8 mb-10 hover:scale-105 items-center justify-center '>
+<div key={item.id} className='bg-indigo-100 flex flex-col w-[60%] hover:cursor-pointer overflow-hidden hover:shadow-[10px_20px_10px_rgba(90,90,90)] md:w-[500px] lg:w-[600px]  sm:w-[400px]  rounded-lg pb-8 mb-10 hover:scale-105 items-center justify-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
 <h3 className='text-[2em]'>{item.title}</h3>
 
 <img src={`${item.image}`} className='w-10/12 rounded-lg ' alt={`${item.title} picture`} />
@@ -35,7 +35,7 @@ return(
 </>
 ))):'loading'}
 <Link to='/add-recipe'>
-<button>Go To Add Page</button>
+<button >Go To Add Page</button>
 </Link>
     </>
 )
