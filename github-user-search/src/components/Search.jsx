18fe689eeb9b78806fd40a52122f617ff1f1fd6b
shalomkerry <shalombadeg8@gmail.com>
2024-12-22 20:28:57 +0300

    someting

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -64,8 +64,11 @@ const Search = () => {
           Submit
         </button>
       </form>
-      
-    {userData.map}
+    {userData && userData.map(location=>{
+        return(<>
+       <p>{location}</p>     
+            </>)
+    })}
       {userData && (
         <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 w-96 text-center">
           <img
